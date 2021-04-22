ba58e5c74e4f1fa08c068e22e59ebba39087c9d6
Fareeha Rana <63462869+FareehaRana@users.noreply.github.com>
2021-04-22 17:47:19 +0500

    weather app completed

---
+++ script/app.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("form");
+const updatecity = async (city) => {
+  const citydetails = await getcity(city);
+  const cityweather = await getweather(citydetails.Key);
+  return { citydetails, cityweather };
+};
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = form.city.value.trim();
+  form.reset();
+  updatecity(city)
+    .then((data) => {
+      //     console.log(data)
+      updateui(data)
+    })
+    .catch((err) => console.log(err));
+});
+const card =document.querySelector(".card")
+const updateui = data => { 
+      const {citydetails,cityweather} = data
+      // console.log(citydetails.EnglishName)
+      // console.log(citydetails.AdministrativeArea.EnglishName)
+      // console.log(citydetails.Country.EnglishName)
+      // console.log(cityweather.IsDayTime)
+      // console.log(cityweather.WeatherIcon)
+      // console.log(cityweather.WeatherText)
+      // console.log(cityweather.Temperature.Metric.Value)
+
+      card.classList.remove("d-none")
+      card.innerHTML = `
+            <img src="img/${cityweather.IsDayTime ? 'day.svg':'night.svg'}" class="card-img-top" alt="..." />
+            <div class="icon bg-light mx-auto">
+              <img src="img/icons/${cityweather.WeatherIcon}.svg" alt=""/>
+            </div>
+            <div>
+              <h4>${citydetails.EnglishName}</h4>
+              <h5>${citydetails.AdministrativeArea.EnglishName}</h5>
+              <h6>${citydetails.Country.EnglishName}</h6>
+              <h6>${cityweather.WeatherText}</h6>
+              <h6>${cityweather.Temperature.Metric.Value} &deg;C</h6>
+            </div>
+ 
+      
+      `
+      
+
+      
+
+}
---
+++ script/forcast.js
@@ -0,0 +1,33 @@
+const apikey = "E4gGAzkFmKZcdEbZA69t0wqAxycIazx3";
+
+const getcity = async (city) => {
+  const baseurl =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${apikey}&q=${city}`;
+  const response = await fetch(baseurl + query);
+  const data = await response.json();
+  return data[0];
+};
+// getcity("faisalabad")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+const getweather = async (citykey) => {
+  const baseurl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${citykey}?apikey=${apikey}`;
+  const response = await fetch(baseurl + query);
+  const data = await response.json();
+  return data[0];
+};
+// getweather(260626)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// getcity("islamabad")
+//   .then((data) => {
+//     return getweather(data.Key);
+//   })
+
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
